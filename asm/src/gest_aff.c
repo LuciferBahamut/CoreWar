2c5971e6c52ccda10ac4f2e956b2c0316e711004
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-05-16 22:18:24 +0200

    [ADD] gest error [FIX] error rule makefile

--- asm/src/gest_aff.c
+++ asm/src/gest_aff.c
@@ -9,5 +9,9 @@
 
 int gest_aff(core_t *core, char *instr, int line)
 {
+    int nbr = get_nbr_args(core->data[line - 1], ',');
+
+    if (nbr != 1)
+        return (display_error(core, line, nbr, 1));
     return (FALSE);
 }
